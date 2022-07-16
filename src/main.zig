47d3c32fa9337fe4f8c26fa0e1e37862b316e779
Colin Trezise <ctrezise@asu.edu>
2022-07-16 15:25:24 -0700

    figured out how to use multiple zig files, also added file for snake data structures

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,14 @@
 const ncurses = @cImport(@cInclude("ncurses.h"));
 const std = @import("std");
 
+const snake = @import("snakeStructs.zig");
+
 pub fn main() anyerror!void {
     _ = ncurses.initscr();
     _ = ncurses.endwin();
+
+    var q = snake.initSnake(3, 3);
+    std.debug.print("snake head x: {}\n", .{q.tail.x});
     // Note that info level log messages are by default printed only in Debug
     // and ReleaseSafe build modes.
     std.log.info("All your codebase are belong to us.", .{});
---
+++ src/snakeStructs.zig
@@ -0,0 +1,33 @@
+pub const Segment = struct {
+    y: u32,
+    x: u32,
+    next: ?*Segment = null,
+};
+
+//snake is implemented as a queue of segments
+pub const Queue = struct {
+    //counter-intuitively, the head of the snake is the tail of the queue and vice versa
+    head: *Segment,
+    tail: *Segment,
+
+    pub fn enqueue(self: *Queue, newSegment: *Segment) void {
+        self.tail.next = newSegment;
+        self.tail = newSegment;
+    }
+
+    //does not free the head, caller must do so
+    pub fn dequeue(self: *Queue) *Segment {
+        var oldHead = self.head;
+        self.head = oldHead.next;
+        return oldHead;
+    }
+};
+
+pub fn initSnake(snakeHeadY: u32, snakeHeadX: u32) Queue {
+    var sHead = Segment{ .y = snakeHeadY, .x = snakeHeadX };
+    var sBody = Segment{ .y = snakeHeadY, .x = snakeHeadX - 1, .next = &sHead };
+    var sTail = Segment{ .y = snakeHeadY, .x = snakeHeadX - 2, .next = &sBody };
+
+    //because of the way the snake moves, the head of the queue is the tail of the snake
+    return Queue{ .head = &sTail, .tail = &sHead };
+}
